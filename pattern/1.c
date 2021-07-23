173c9e934396bcc87d9f84ae6fa542a5ad5428ea
Abhishek Morla <56071093+abhishekmorla@users.noreply.github.com>
2021-07-24 02:01:35 +0530

    Add files via upload

---
+++ pattern/1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(){
+    printf("Hello world!\n");
+    int n;
+    scanf("%d",&n);
+    for (int i = 1 ; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+             if (j == 1 || i == n || i == 1 || j == n)
+             {
+                 printf("*");
+             }
+             else{
+                 printf(" ");
+             }
+        }
+        printf("\n");  
+    }
+    return 0;
+}
