d7c367b87c1da77f1c5ad4c685256a202b1d2fbc
Abhishek Morla <56071093+abhishekmorla@users.noreply.github.com>
2022-01-20 04:50:56 -0500

    Add files via upload

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,34 +1,49 @@
-#include <iostream>
-
+#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int getmax(vector<int>& arr , int start , int end )
+{
+    int max=start;
+    for (int i = start; i <= end; i++)
+    {
+        if( arr[max] < arr[i] )
+        {
+            
+            max = i;
+        }
+    }
+    return max;
+}
+
+void swap (vector<int>& arr, int a , int b)
+{
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+void selectionsort(vector<int>& arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        int last = arr.size() - i - 1;
+        int maxind = getmax(arr , 0 , last);
+        swap(arr , maxind , last);
+        
+    }
+
+}
 
 int main()
 {
-	int n,j;
-	cin>>n;
-	int ar[n];
-	for (int k = 0 ; k < n ; k++)
-	{
-		cin>>ar[k];
-	}
-	// sorting
-	for (int i = 0; i < n-1; i++)
-	{
-		int min = i;
-		for (j = i+1; i < n; j++)
-		{
-			if(ar[j] < ar[min])
-			{
-				min = j;
-			}
-		}
-		int temp = ar[i];
-		ar[i] = ar[min];
-		ar[min] = temp;
-	}
-	for (int p = 0 ; p < n ; p++)
-	{
-		cout<<ar[p];
-	}
-
+    vector <int> arr = {12,3,2,31,4,6};
+    selectionsort(arr); 
+    
+    
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    return 0;
 }
